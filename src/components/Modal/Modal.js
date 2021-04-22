4eba650ffdf2b96251906eb5e4812a4820001746
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-22 14:33:54 -0600

    updating Modal and Programs container

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {forwardRef} from "react";
 import "./Modal.scss";
 
 export function ModalPadding({ children = null, className = "", ...props }) {
@@ -9,7 +9,7 @@ export function ModalPadding({ children = null, className = "", ...props }) {
   );
 }
 
-export default React.forwardRef(Modal);
+export default forwardRef(Modal);
 
 function Modal(props, ref) {
   const {
@@ -27,7 +27,7 @@ function Modal(props, ref) {
           {Boolean(header) && (
             <header className="modal-structure">{header}</header>
           )}
-          {Boolean(content) && <div className="modal-structure">{content}</div>}
+          {Boolean(content) && <div className="modal-structure">Looking to get 1 on 1 work? No problem. We can work on specific drills and skills that you feel that you could work on. We can do a personal assessment of specific areas that need to be worked on.</div>}
           {Boolean(footer) && (
             <footer className="modal-structure">{footer}</footer>
           )}
