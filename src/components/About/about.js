9d5948ff00fd8ba6fd4c31dea1dbd4ef4600a72d
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-22 19:00:16 -0600

    updating about section and responsiveness

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -13,7 +13,7 @@ const about = () => {
               <div className="content-column col-lg-6 col-md-12 col-sm-12 order-2">
                   <div className="inner-column">
                       <div className="sec-title">
-                          <h2> <br/>About the Owner</h2>
+                          <h2> <br/>ABOUT THE OWNER</h2>
                       </div>
                       <div className="text">
                         <p>
@@ -65,11 +65,6 @@ const about = () => {
                   Pariatur aliqua nostrud anim dolore adipisicing et sit aute commodo ad sint aliquip amet ex. Consequat deserunt ea dolor minim nisi aliquip ex voluptate. Consequat consectetur duis est in mollit veniam nostrud fugiat eu ea non cillum. Ullamco nisi consequat consequat mollit occaecat et sit velit nisi voluptate ipsum. Ipsum elit do do ullamco anim elit enim ipsum. Officia voluptate minim consectetur ullamco esse aliqua veniam dolor nulla sit enim veniam amet. 
                 </p>
               </div>
-                <ul className="list-style-one">
-                    <li>Lorem Ipsum is simply dummy tex</li>
-                    <li>Consectetur adipisicing elit</li>
-                    <li>Sed do eiusmod tempor incididunt</li>
-                </ul>
                 <div className="btn-box">
                     <a href="/register" className="theme-btn btn-style-one">JOIN US</a>
                 </div>
@@ -77,7 +72,10 @@ const about = () => {
             </div>
 
             <div className="image-column col-lg-6 col-md-12 col-sm-12">
-
+              <div className="inner-column wow fadeInLeft">
+                <figure className="image-1"><a href="https://google.com" className="lightbox-image" data-fancybox="images"><img src="https://bloximages.newyork1.vip.townnews.com/collegian.psu.edu/content/tncms/assets/v3/editorial/b/be/bbe2f5be-0336-11ea-9f81-938a70565b21/5dc72d9459056.image.jpg?resize=1200%2C1241" alt=""/></a></figure>
+                <figure className="image-2"><a href="https://google.com" className="lightbox-image" data-fancybox="images"><img src="https://cdn.vox-cdn.com/thumbor/CAhU7JXkEyPAW9I5iFwEscXcnBE=/0x0:4026x2684/1200x800/filters:focal(2376x288:3020x932)/cdn.vox-cdn.com/uploads/chorus_image/image/54440957/usa_today_9555841.0.jpg" alt=""/></a></figure>
+              </div>
             </div>
           </div>
         </div>
