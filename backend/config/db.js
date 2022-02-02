17841f66ce8d529fab49e85451043d2cabba0316
mosesvk <kaumatule93@gmail.com>
2022-02-01 21:55:26 -0700

    fixed MongoDB connection issue. Had to make sure the .env file was connected through dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
