f0303221f3861f7e2ac857f5928b8ed0226b74a1
mosesvk <kaumatule93@gmail.com>
2022-02-08 13:01:43 -0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,23 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+// Prepare for Deployment
+// making the uploads root folder static
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 //errorMiddleware.js
 app.use(notFound);
 app.use(errorHandler);
