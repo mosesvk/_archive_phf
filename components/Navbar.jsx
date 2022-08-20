80bb3e285a117691b5ab7c562daefd5cbfa5d488
mosesvk <kaumatule93@gmail.com>
2022-08-20 15:49:19 -0600

    updating navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <Sidebar />
       <nav className='block px-8 py-4 bg-gray-600 text-white h-24 sticky top-0 z-100 box-content'>
         <div className='flex justify-between items-center h-full xl:px-56 lg:px-32 px-12'>
-          <a className='text-5xl font-extrabold font-["mainLogo"] max-w-sm text-center text-slate-50 hover:decoration-0' href='/'>
+          <a className='text-5xl font-extrabold fontFamily-["mainLogo"] max-w-sm text-center text-slate-50 hover:decoration-0' href='/'>
             <h1 className='text-4xl md:text-2xl sm:text-lg'>POWERHOUSE FITNESS</h1>
           </a>
           <div className='nav-info-wrap'>
